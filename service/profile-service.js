87951d3dfbb42605a0baa27d901cfb1688b87bb0
NickyL279 <60162477+NickyL279@users.noreply.github.com>
2021-11-16 18:24:45 -0500

    first commit

---
+++ service/profile-service.js
@@ -0,0 +1,23 @@
+let profile = require('../data/profile.json');
+
+module.exports = (app) => {
+    const getCurrentProfile = (req, res) => res.json(profile);
+    app.get('/api/profile', getCurrentProfile);
+
+    const updateCurrentProfile = (req, res) => {
+        console.log("req.body" + req.body);
+        console.log("JSON req.body- " + JSON.stringify(req.body));
+        console.log("JSON req.body.name - " + req.body.nameNew);
+        profile = {
+            ...profile,
+            name: req.body.nameNew,
+            bio: req.body.bioNew,
+            location: req.body.locationNew,
+            website: req.body.websiteNew,
+            dateOfBirth: req.body.dobNew
+        }
+        console.log("New profile- " + JSON.stringify(profile));
+        res.json(profile);
+    }
+    app.put('/api/profile', updateCurrentProfile);
+};
